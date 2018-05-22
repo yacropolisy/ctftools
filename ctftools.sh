d1dc05490c715086a0b0b9c3de5b99d2f66d3ebe
Yoshinobu Yamada <cancer_acropolis@yahoo.co.jp>
2018-05-22 01:44:19 +0000

    [add] pyenv

--- ctftools.sh
+++ ctftools.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y build-essential gcc-multilib gdb
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
 # install rp++
-export PATH=$HOME/bin:$PATH
+echo "export PATH=\$HOME/bin:\$PATH" >> ~/.bash_profile
 mkdir $HOME/bin
 wget https://github.com/downloads/0vercl0k/rp/rp-lin-x64 -O $HOME/bin/rp
 # install checksec.sh
@@ -26,3 +26,10 @@ tar zxvf 1.6.tar.gz -C $HOME  && rm 1.6.tar.gz
 mv checksec.sh-1.6/checksec $HOME/bin/checksec
 # install socat
 sudo apt-get install socat
+# install pyenv
+sudo apt-get -y install build-essential libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl libbz2-dev libreadline-dev
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
